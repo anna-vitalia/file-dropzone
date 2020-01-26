def06129d8b62ec3b420be68e526b2e744a07603
unknown <kublitskayavd@yandex.ru>
2020-01-26 23:10:54 +0300

    добавлены проверки на размер и формат, выведен список файлов

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,22 +5,28 @@ const Form = () => {
     const {acceptedFiles, getContainerProps, getInputProps} = FileDropzone();
     
     let files = acceptedFiles.map(file => (
-      <div key={file.name}>
-        {file.name} - {file.size} bytes
+      <div className='item' key={file.name}>
+        <div className='info'>
+          <span className='name'>{file.name}</span> <span className='size'>{file.size}</span> bytes
+        </div>
       </div>
     ));
 
     return (
-      <div>
+      <div className='files-zone'>
         <div className="dropzone" {...getContainerProps()}>
-        <input {...getInputProps()} />
-        <div className="label">
-          <p>drag and drop here your files </p>
-          {files}
+          <input {...getInputProps()} />
+          <div className="label">
+            <p>drag and drop here your files </p>
+          </div>
+        </div>
+        <div className='files-wrapper'>
+          <div>Files</div>
+          <div className='files-list'>
+            {files}
+          </div>
         </div>
       </div>
-      </div>
-      
     );
 }
 
--- src/components/utils/FileDropzone.jsx
+++ src/components/utils/FileDropzone.jsx
@@ -1,8 +1,30 @@
 import React, {useCallback, useRef, useMemo, useReducer} from 'react';
 
+const initialState = {
+  acceptedFiles: [],
+  rejectedFiles: []
+}
+
+
 function reducer(state, action) {
   switch (action.type) {
     case 'setAcceptedFiles':
+      return {
+        ...state,
+        acceptedFiles: action.acceptedFiles,
+      }
+    case 'setRejectedFiles':
+      return {
+        ...state,
+        rejectedFiles: action.rejectedFiles,
+      }
+    case 'setAllFiles':
+      return {
+        ...state,
+        acceptedFiles: action.acceptedFiles,
+        rejectedFiles: action.rejectedFiles,
+      }
+    case 'deleteFile':
       return {
         ...state,
         acceptedFiles: action.acceptedFiles,
@@ -12,34 +34,54 @@ function reducer(state, action) {
   }
 }
 
-const initialState = {
-  allLoadedFiles: [],
-  acceptedFiles: [],
-  invalidFiles: []
+function isDefined(value) {
+  return value !== undefined && value !== null
 }
 
-export function FileDropzone({multiple = true} = {}) {
+export function FileDropzone({
+  multiple = true,
+  maxSize = Infinity,
+  minSize = 0,
+  acceptFormats,
+  rejectFormats,
+  onDragEnter,
+  onDragLeave,
+  onDragOver,
+  onDrop,
+} = {}) {
 
   const refDropzone = useRef(null);
   const refInput = useRef(null);
   const [state, dispatch] = useReducer(reducer, initialState);
-  //const fileFilter = {};
 
-  const handleOnDrag = useCallback( event => {
+  const handleOnDragEnter = useCallback( event => {
     event.preventDefault();
+    if (onDragEnter) {
+      onDragEnter(event);
+    }
+  }, [] );
+
+  const handleOnDragOver = useCallback( event => {
+    event.preventDefault();
+    if (onDragOver) {
+      onDragOver(event);
+    }
   }, [] );
 
   const handleOnDrop = useCallback( event => {
     event.preventDefault();
     let dataTransfer = event.dataTransfer;
     let files = dataTransfer.files;
-    changeFilesList(files);
+    addFiles(files);
+    if (onDrop) {
+      onDrop(event);
+    }
   }, [] );
 
   const handleOnChange = useCallback( event => {
     event.preventDefault();
     let files = event.target.files;
-    changeFilesList(files);
+    addFiles(files);
   }, [] );
 
   const handleOnClick = useCallback( event => {
@@ -53,31 +95,63 @@ export function FileDropzone({multiple = true} = {}) {
     }
   }, [] );
 
-  const changeFilesList = (files) => {
+  const addFiles = (files) => {
+    let currentFiles = (multiple) ? state.acceptedFiles : [];
     let acceptedFiles = [];
+    let rejectedFiles = [];
     for (var i = 0; i < files.length; i++) {
-      acceptedFiles.push(files[i]);
+      currentFiles.push(files[i]);
     }
-    dispatch({
-      acceptedFiles,
-      type: 'setAcceptedFiles'
+    currentFiles.forEach(file => {
+      if (fileAccepted(file) && fileMatchSize(file)) {
+        acceptedFiles.push(file)
+      } else {
+        rejectedFiles.push(file)
+      }
     })
-    console.log(state.acceptedFiles);
+    dispatch({
+      type: 'setAllFiles',
+      acceptedFiles,
+      rejectedFiles
+    });
   };
+
+  const fileAccepted = (file) => {
+    if (isDefined(file.format)) {
+      if (acceptFormats.length > 0 && rejectFormats.length > 0) {
+        return (acceptFormats.indexOf(file.format) && !rejectFormats.indexOf(file.format));
+      } else if (acceptFormats.length > 0) {
+        return (acceptFormats.indexOf(file.format));
+      } else if (rejectFormats.length > 0) {
+        return (!rejectFormats.indexOf(file.format));
+      }
+    }
+    return true;
+  }
+
+  const fileMatchSize = (file) => {
+    if (isDefined(file.size)) {
+      if (minSize >= 0 && maxSize)
+        return file.size >= minSize && file.size <= maxSize
+      else if (minSize >= 0)
+        return file.size >= minSize
+      else if (maxSize)
+        return file.size <= maxSize
+    }
+    return true;
+  }
 
   const getContainerProps = useMemo(() => () => {
     const props = {
       ref: refDropzone,
       onClick: handleOnClick,
-      onDragEnter: handleOnDrag,
-      onDragOver: handleOnDrag,
+      onDragEnter: handleOnDragEnter,
+      onDragOver: handleOnDragOver,
       onDrop: handleOnDrop,
     };
 
-    return (
-      props
-    )
-  }, [refDropzone, handleOnClick, handleOnDrag, handleOnDrop]);
+    return ( props );
+  }, [refDropzone, handleOnClick, handleOnDragEnter, handleOnDragOver, handleOnDrop]);
   
   const getInputProps = useMemo(() => () => {
     const props = {
@@ -87,9 +161,7 @@ export function FileDropzone({multiple = true} = {}) {
       onChange: handleOnChange
     };
 
-    return (
-      props
-    )
+    return ( props )
   }, []);
 
   return {
